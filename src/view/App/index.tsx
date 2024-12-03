2fed160f847113a3debedf2092c2aa827f0f7c30
chenh <chenh@kmsoft.com.cn>
2024-12-03 15:02:49 +0800

    Better Windows10 border

--- src/view/App/index.tsx
+++ src/view/App/index.tsx
@@ -87,7 +87,7 @@ const style = () => {
       top: 0,
       right: 0,
       bottom: 0,
-      border: '1px solid rgba(0, 0, 0, 0)',
+      border: '1px solid rgba(40, 50, 60, 0.4)',
       minWidth: '700px',
       minHeight: '500px',
       lineHeight: '16px',
